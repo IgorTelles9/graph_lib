d6fd20dc27919b12f94e03fa154b62acf7465a75
Igor Telles <igortelles@ibm.com>
2021-12-21 21:03:03 -0300

    add diameter, distance, median and average

--- graph.py
+++ graph.py
@@ -1,4 +1,5 @@
 from queue import Queue
+from math import ceil 
 class Graph: 
 
     def __init__(self, file, list = True) -> None:
@@ -7,6 +8,7 @@ class Graph:
         self._degree = []
         self._graph = []
         self._edges = 0
+        self._level = []
 
         with open(file) as reader:
             line  = reader.readline()
@@ -70,6 +72,18 @@ class Graph:
                     line = reader.readline()
 
 
+    def getAverageDegree(self):
+        return sum(self._degree)/self._vertices
+
+    def getMedianDegree(self):
+        degree_sorted = sorted(self._degree)
+        if (self._vertices % 2 == 0):
+            x = degree_sorted[ ceil(self._vertices/2) - 1]
+            y = degree_sorted[ ceil(self._vertices/2)]
+            return (x+y)/2
+        return degree_sorted[ ceil(self._vertices/2) - 1]
+
+
     def getInfo(self, file):
         """ 
         Creates a .txt file with the following information: 
@@ -82,16 +96,18 @@ class Graph:
             writer.write('Numero de arestas: ' + str(self._edges) + '\n')
             writer.write('Grau maximo: ' + str(max(self._degree)) + '\n' )
             writer.write('Grau minimo: ' + str(min(self._degree)) + '\n' )
+            writer.write('Grau médio: ' + "{:.2f}".format(self.getAverageDegree()) + '\n' )
+            writer.write('Mediana do grau: ' + "{:.1f}".format(self.getMedianDegree()) + '\n' )
     
-    def bfs(self, s):
+    def bfs(self, s, w=True):
         """ Executes a bfs, starting in the given vertex s. Return a txt file"""
         marker = [0 for x in range(len(self._graph))]
-        level = [-1 for x in range(len(self._graph))]
+        self._level = [-1 for x in range(len(self._graph))]
         queue = Queue() 
         queue_removed = []
         queue.push(s)
         marker[s-1] = 1
-        level[s-1] = 0
+        self._level[s-1] = 0
         arvore =[]
 
         arvore.append([f"Vértice {s} - Pai: Não tem, Nível: 0"])
@@ -103,26 +119,49 @@ class Graph:
                 for item in self._graph[u-1]:
                     if marker[item-1] == 0:
                         marker[item-1] = 1
-                        level[item-1] = level[u-1] + 1
-                        arvore.append([f"Vértice {item} - Pai: {u}, Nível: {level[item-1]}"])
+                        self._level[item-1] = self._level[u-1] + 1
+                        arvore.append([f"Vértice {item} - Pai: {u}, Nível: {self._level[item-1]}"])
                         queue.push(item)
             else:
                 for index, item in enumerate(self._graph[u-1]):
                     if item == 1:
                         if marker[index] == 0:
                             marker[index] = 1
-                            level[index] = level[u-1] + 1
-                            arvore.append([f"Vértice {index + 1} - Pai: {u}, Nível: {level[index]}"])
+                            self._level[index] = self._level[u-1] + 1
+                            arvore.append([f"Vértice {index + 1} - Pai: {u}, Nível: {self._level[index]}"])
                             queue.push(index + 1)
 
-        with open('bfs.txt', 'w')  as writer:
-            for item in arvore:
-                for text in item:
-                    writer.write(text + '\n')
+        if (w):
+            with open('bfs.txt', 'w')  as writer:
+                for item in arvore:
+                    for text in item:
+                        writer.write(text + '\n')
         
+    def getDistance(self,a,b, w=True):
+        self.bfs(a, False)
+        if (w):
+            with open('distance_' + str(a) + '_' + str(b) + '.txt', 'w') as writer:
+                writer.write(str(self._level[b-1]))
+        else:
+            return self._level[b-1]
+
+    def getDiameter(self, w=True):
+        diameter = 0
+        for vertex_a in range(self._vertices):
+            self.bfs(vertex_a-1, False)
+            diameter = max(self._level) if (max(self._level) > diameter) else diameter
+            
+        if (w):
+            with open('diameter.txt', 'w') as writer:
+                writer.write(str(diameter))
+        else:
+            return diameter   
+      
 g = Graph('grafo_1.txt')
 g.getInfo('exit.txt')
 g.bfs(1)
+g.getDistance(1,3)
+g.getDiameter()
 
                 
                 
